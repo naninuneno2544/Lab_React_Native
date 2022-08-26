d0324a5bd0627bbf8099db60742ed72f4bb12cdf
tanawat <pirotover1@gmail.com>
2022-08-26 15:01:13 +0700

    ข้อ2 Hello World(ปรับปรุง)

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants'
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
- );
+    const doIt = () => {
+        console.log("Hello from console")
+    }
+    return (
+        <View style={styles.container}>
+            <Weather zipCode="90110" />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
- },
+    container: {
+        paddingTop: Constants.statusBarHeight
+    },
 });
 
