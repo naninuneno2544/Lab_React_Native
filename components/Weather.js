d06f13aec8ff785aceb5dcd9105e6b78809e301b
tanawat <pirotover1@gmail.com>
2022-08-26 16:28:23 +0700

    ข้อ4 Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=8a21e23b7e62ab8d3cb8995c0c73f52b`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+    
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
